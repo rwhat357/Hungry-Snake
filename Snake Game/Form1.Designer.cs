08b1b96c7f42ce258bdbd7b39f9cdf431f199e7b
Fredy Whatley <rwhat357@hotmail.com>
2015-01-09 00:08:18 -0500

    Completed hungry snake game.

---
+++ Snake Game/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Snake_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.snakeScoreLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.spaceBarLabel = new System.Windows.Forms.Label();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.snakeScoreLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 299);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(290, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(42, 17);
+            this.toolStripStatusLabel1.Text = "Score: ";
+            // 
+            // snakeScoreLabel
+            // 
+            this.snakeScoreLabel.Name = "snakeScoreLabel";
+            this.snakeScoreLabel.Size = new System.Drawing.Size(13, 17);
+            this.snakeScoreLabel.Text = "0";
+            // 
+            // spaceBarLabel
+            // 
+            this.spaceBarLabel.AutoSize = true;
+            this.spaceBarLabel.Location = new System.Drawing.Point(74, 126);
+            this.spaceBarLabel.Name = "spaceBarLabel";
+            this.spaceBarLabel.Size = new System.Drawing.Size(131, 13);
+            this.spaceBarLabel.TabIndex = 1;
+            this.spaceBarLabel.Text = "Press Space Bar to Begin.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(290, 321);
+            this.Controls.Add(this.spaceBarLabel);
+            this.Controls.Add(this.statusStrip1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Hungry Snake";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel snakeScoreLabel;
+        private System.Windows.Forms.Label spaceBarLabel;
+    }
+}
+
---
+++ Snake Game/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Snake_Game
+{
+    public partial class Form1 : Form
+    {
+        Random randFood = new Random();
+        Graphics paper;
+        Snake snake = new Snake();
+        Food food;
+
+        int score = 0;
+
+        // directions
+        bool left = false;
+        bool right = false;
+        bool down = false;
+        bool up = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            food = new Food(randFood);
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            paper = e.Graphics;
+            food.drawFood(paper);
+            snake.drawSnake(paper);
+
+
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Space)
+            {
+                timer1.Enabled = true;
+                spaceBarLabel.Text = "";
+                down = false;
+                up = false;
+                right = true;
+                left = false;
+            }
+
+            if (e.KeyData == Keys.Down && up == false)
+            {
+                down = true;
+                up = false;
+                right = false;
+                left = false;
+            }
+
+            if (e.KeyData == Keys.Up && down == false)
+            {
+                down = false;
+                up = true;
+                right = false;
+                left = false;
+            }
+
+            if (e.KeyData == Keys.Right && left == false)
+            {
+                down = false;
+                up = false;
+                right = true;
+                left = false;
+            }
+
+            if (e.KeyData == Keys.Left && right == false)
+            {
+                down = false;
+                up = false;
+                right = false;
+                left = true;
+            } 
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            snakeScoreLabel.Text = Convert.ToString(score);
+
+            if (down) { snake.moveDown(); }
+            if (up) { snake.moveUp(); }
+            if (right) { snake.moveRight(); }
+            if (left) { snake.moveLeft(); }
+
+            // eat food on collision
+            for (int i = 0; i < snake.SnakeRec.Length; i++)
+            {
+                if (snake.SnakeRec[i].IntersectsWith(food.foodRec))
+                {
+                    score += 10;
+                    snake.growSnake();
+                    timer1.Interval -= 5;
+                    food.foodLocation(randFood); // place a new random food at random place
+                }
+            }
+
+            collision();
+            this.Invalidate(); //  tell the paint function to redraw what is in the form.
+        }
+
+        public void collision()
+        {
+
+            // collision on snake hitting his own body
+            for (int i = 1; i < snake.SnakeRec.Length; i++)
+            {
+                if (snake.SnakeRec[0].IntersectsWith(snake.SnakeRec[i]))
+                {
+                    restart();
+                }
+            }
+
+            // on left and right collision
+            if (snake.SnakeRec[0].X < 0 || snake.SnakeRec[0].X > 290)
+            {
+                restart();
+            }
+
+            // on up and down collision
+            if (snake.SnakeRec[0].Y < 0 || snake.SnakeRec[0].Y > 290)
+            {
+                restart();
+            }
+
+        }
+
+        public void restart()
+        {
+            timer1.Enabled = false;
+            timer1.Interval = 50;
+            MessageBox.Show("Snake is dead! You scored " + score + " points.");
+            snakeScoreLabel.Text = "0";
+            score = 0;
+
+            spaceBarLabel.Text = "Press Space Bar to Begin.";
+            snake = new Snake();
+        }
+
+    }
+}
